e14216319b636d9165c8b00badb61e5e27c6599d
divyaa_sinhaa <divyasinha633@gmail.com>
2024-07-14 19:42:31 +0530

    first commit

---
+++ Programs/first.java
@@ -0,0 +1,9 @@
+package Programs;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("HEYYYYY I WAS DOING FINE BEFORE I MET YOU");
+        System.out.println("HEYY");
+        System.out.println("THIS IS WHAT IT IS");
+    }
+}
